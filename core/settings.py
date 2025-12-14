f47d51f2a53f19383978e4f078beface70b49a03
sawantsanket855 <sawantsanket855@gmail.com>
2025-12-14 09:23:42 +0530

    complete flow render

--- core/settings.py
+++ core/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "payment_manager",      # your local DB name
-        "USER": "postgres",          # your PostgreSQL username
-        "PASSWORD": "Sanket@2146",          # your PostgreSQL password
-        "HOST": "localhost",         # or 127.0.0.1
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql",
+#         "NAME": "payment_manager",      # your local DB name
+#         "USER": "postgres",          # your PostgreSQL username
+#         "PASSWORD": "Sanket@2146",          # your PostgreSQL password
+#         "HOST": "localhost",         # or 127.0.0.1
+#         "PORT": "5432",
+#     }
+# }
 
 # DATABASES = {
 #     "default": dj_database_url.config(
